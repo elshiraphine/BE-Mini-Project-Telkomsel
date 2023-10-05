74bbc540daf727664d7d4333f0faa11ee193386c
gybran <charistomarcgybran@students.undip.ac.id>
2023-10-05 15:30:11 +0700

    add frontend

--- app.js
+++ app.js
@@ -1,28 +1,24 @@
 const express = require("express");
+const path = require("path");
 const app = express();
-const bodyParser = require("body-parser");
-const cors = require("cors");
-const cookieParser = require("cookie-parser");
-var logger = require("morgan");
-app.use(cors());
 
-app.use(logger("dev"));
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(cookieParser());
-
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-    res.status(200).send({
-      msg: "Wellcome to mini project API",
-    });
+    res.send("<h1>Hello World!</h1>");
 });
+
+app.get("/calculate", (req, res) => {
+    res.render("index", { luas_segitiga: null });
+});
+app.use(require("./routes/indexRoute"));
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
     console.log(`Server running on port http://localhost:${port}`);
-    }
-);
+});
 
 module.exports = app;
-
--- routes/indexRoute.js
+++ routes/indexRoute.js
@@ -1,8 +1,16 @@
 //require controllers
-const indexController = require("../controllers/indexController");
+const indexController = require("../controllers/index");
 const express = require("express");
 const router = express.Router();
 
-router.post("/Count", indexController.register);
+router.post("/calculate", (req, res) => {
+    try {
+        const { alas_segitiga, tinggi_segitiga } = req.body;
+        const luas_segitiga = (alas_segitiga * tinggi_segitiga) / 2;
+        res.render("index", { luas_segitiga });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
 
 module.exports = router;
