0565a3b2c4eadab1f59721b79641d6ea663c93fb
Benhard Sim <36811298+BenhardSim@users.noreply.github.com>
2023-10-16 21:28:38 +0700

    add lambda but still via aws gw

--- app.js
+++ app.js
@@ -1,24 +1,36 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+const AWS = require('aws-sdk');
+
+
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-    res.send("<h1>Hello World!</h1>");
-});
+AWS.config.update({ region: 'ap-southeast-3' }); // Replace 'your-region' with the AWS region where your Lambda function is deployed.
 
-app.get("/calculate", (req, res) => {
+const lambda = new AWS.Lambda();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
     res.render("index", { luas_segitiga: null });
 });
+
+// app.post("/compute", (req, res) => {
+
+// });
+
 app.use(require("./routes/indexRoute"));
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
     console.log(`Server running on port http://localhost:${port}`);
 });
+
+
 
 module.exports = app;
